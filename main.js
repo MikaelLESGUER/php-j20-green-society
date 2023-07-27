7b1395a955cb821fb573a6525b4fef102067ea10
Mikael LESGUER <mikedev35@outlook.fr>
2023-07-27 16:48:50 +0200

    mise en dossier

--- main.js
+++ main.js
@@ -20,7 +20,25 @@ function startSlider() {
 // Démarrage du slider
 startSlider();
 
-    // Exemple d'utilisation de WAI-ARIA pour décrire un bouton
-    const buttonElement = document.querySelector('button');
-    buttonElement.setAttribute('role', 'button');
-    buttonElement.setAttribute('aria-label', 'Cliquez ici pour effectuer une action');
+// Exemple d'utilisation de WAI-ARIA pour décrire un bouton
+const buttonElement = document.querySelector('button');
+buttonElement.setAttribute('role', 'button');
+buttonElement.setAttribute('aria-label', 'Cliquez ici pour effectuer une action');
+
+const sliderMain = document.querySelectorAll('.slider-main');
+let mainIndex = 0;
+
+// Fonction pour afficher l'image suivante dans le slider
+function showNextSlide() {
+    sliderMain[mainIndexIndex].classList.remove('active');
+    mainIndex = (mainIndex + 1) % sliderMain.length;
+    sliderMain[mainIndexIndex].classList.add('active');
+}
+
+// Démarre le slider automatique
+function startSlider() {
+    setInterval(showNextSlide, 5000); // Change d'image toutes les 5 secondes (5000 ms)
+}
+
+// Démarrage du slider
+startSlider();
